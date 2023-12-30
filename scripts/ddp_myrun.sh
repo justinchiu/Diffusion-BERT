ff46ea8bf641b32544af5312080e01b3788d6f07
Justin Chiu <chiu.justin.t@gmail.com>
2023-12-30 02:49:19 -0500

    cant trust their training restarts. dont allow

--- scripts/ddp_myrun.sh
+++ scripts/ddp_myrun.sh
@@ -2,5 +2,4 @@ torchrun --nnodes=1 --nproc-per-node=4 --master-port=29400 DDP_main.py \
   --lr 5e-5 \
   --batch_size 32 \
   --timestep 'layerwise' \
-  --from_scratch false \
-  --load_step 164999
+  --from_scratch false
