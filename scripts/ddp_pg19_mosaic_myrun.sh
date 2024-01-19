78e89b658df6ed8f5c8f8d29e9ba2957c250ef11
Justin Chiu <chiu.justin.t@gmail.com>
2024-01-19 02:03:03 -0500

    run DDP

--- scripts/ddp_pg19_mosaic_myrun.sh
+++ scripts/ddp_pg19_mosaic_myrun.sh
@@ -1,5 +1,5 @@
-#torchrun --nnodes=1 --nproc-per-node=4 --master-port=29400 DDP_main.py \
-torchrun --nnodes=1 --nproc-per-node=1 --master-port=29400 DDP_main.py \
+torchrun --nnodes=1 --nproc-per-node=4 --master-port=29400 DDP_main.py \
+#torchrun --nnodes=1 --nproc-per-node=1 --master-port=29400 DDP_main.py \
   --task_name pg19 \
   --max_length 1024 \
   --lr 5e-5 \
