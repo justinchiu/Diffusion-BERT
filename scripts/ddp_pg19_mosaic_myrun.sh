65cc97216d73c3eb6aaa33d879bdb56a5645aa83
Justin Chiu <chiu.justin.t@gmail.com>
2024-02-11 22:47:32 -0500

    add args to pg19 mosaicbert diffusion

--- scripts/ddp_pg19_mosaic_myrun.sh
+++ scripts/ddp_pg19_mosaic_myrun.sh
@@ -1,14 +1,14 @@
 #torchrun --nnodes=1 --nproc-per-node=2 --master-port=29402 DDP_main.py \
-torchrun --nnodes=1 --nproc-per-node=1 --master-port=29401 DDP_main.py \
+torchrun --nnodes=1 --nproc-per-node=1 --master-port=$3 DDP_main.py \
   --task_name pg19 \
-  --max_length 512 \
+  --max_length $1 \
   --lr 5e-5 \
   --batch_size 32 \
   --timestep 'layerwise' \
   --model_name_or_path mosaicml/mosaic-bert-base-seqlen-2048 \
   --wrap_text \
   --epochs 10 \
-  --num_steps 2048 \
+  --num_steps $2 \
   --logging_steps 100
 # --batch_size 16 for a100
 # --batch_size 4 for rush-compute-01
